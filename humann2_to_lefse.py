a753e430bb9baad7bdeb2c7fe5a09f1ee4a1471e
Jon Sanders <jsanders@oeb.harvard.edu>
2016-06-18 14:46:53 -0700

    fixed bug

--- humann2_to_lefse.py
+++ humann2_to_lefse.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import print_function
 """
 Takes a QIIME sample metadata mapping file and a Humann2 output table and makes
 a lefse-compatable file using the supplied class and subclass categories. 
