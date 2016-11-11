1b682f6c43a3dba93bbce48f81a20026fdff3285
Jon Sanders <jsanders@oeb.harvard.edu>
2016-11-11 11:08:02 -0500

    added output directory specification

--- split_biom.py
+++ split_biom.py
@@ -3,6 +3,7 @@ import argparse
 from biom import load_table
 from h5py import File
 from biom.util import biom_open
+from os.path import join
 
 parser = argparse.ArgumentParser()
 
@@ -10,12 +11,15 @@ parser.add_argument("-n", "--n", type=int,
                     help="split biom file into n biom files")
 parser.add_argument("-i", "--input_fp",
                     help="biom file to split")
+parser.add_argument("-o", "--output_dir",
+                    help="dir to put output split biom files")
+
 
 def main():
     args = parser.parse_args()
     n = args.n
     input_fp = args.input_fp
-
+    outdir = args.output_dir
 
     biom_table = load_table(input_fp)
 
@@ -36,7 +40,7 @@ def main():
         sub_ids = obs_ids[begin_id : end_id]
 
         sub_table = biom_table.filter(lambda val, id_, md: id_ in sub_ids, axis='observation', invert=False, inplace=False)
-        with biom_open('chunk{0}.biom'.format(chunk), 'w') as out_f:
+        with biom_open(join(out_dir,'chunk{0}.biom'.format(chunk)), 'w') as out_f:
             sub_table.to_hdf5(out_f, "split_biom.py")
 
         last_id = end_id
@@ -49,7 +53,7 @@ def main():
     sub_ids = obs_ids[last_id + 1 : ]
 
     sub_table = biom_table.filter(lambda val, id_, md: id_ in sub_ids, axis='observation', invert=False, inplace=False)
-    with biom_open('chunk{0}.biom'.format(n), 'w') as out_f:
+    with biom_open(join(out_dir,'chunk{0}.biom'.format(n)), 'w') as out_f:
         sub_table.to_hdf5(out_f, "split_biom.py")
 
 
